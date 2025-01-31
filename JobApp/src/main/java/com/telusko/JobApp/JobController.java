8aa426d03d4b8eddeb57fe627b9e9b52b5ee60cd
sajansuvarnan <sajan.suvarnan@gmail.com>
2025-01-31 18:45:13 +0530

    added home page

---
+++ JobApp/src/main/java/com/telusko/JobApp/JobController.java
@@ -0,0 +1,14 @@
+package com.telusko.JobApp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JobController {
+
+    @RequestMapping({"/","home"})
+    public String home()
+    {
+        return "home";
+    }
+}
